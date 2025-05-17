4ae2021fd2aad402c86be1cc8aa50880f6f9834e
Maths-and-Physics-Are-Fun <CD_au@yahoo.com>
2025-05-17 17:41:09 +1000

    Feedforward

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/teleOp/FeedForwardTuner.java
@@ -0,0 +1,16 @@
+package org.firstinspires.ftc.teamcode.opModes.teleOp;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+public class FeedForwardTuner extends OpMode {
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
+}
